4502533fcf01d95f63aa7d2f896090417a14af18
Alireza Ahmadi <77675646+AwlirezaZ@users.noreply.github.com>
2023-02-25 01:48:48 +0330

    add prime number solution

---
+++ PAP Learning/PrimeNumber/PrimeNumber/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrimeNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number");
+            int number = int.Parse(Console.ReadLine());
+            if (IsPrimeNumber(number)) Console.WriteLine($"{number} is prime number");
+            else Console.WriteLine($"{number} is not prime number");
+            Console.ReadKey();
+           
+            bool IsPrimeNumber(int num)
+            {
+                for (int counter = 2 ; counter < num; counter++)
+                {
+                    if (num % counter == 0) return false;
+                }
+                return true;
+            }
+        }
+
+    }
+}
